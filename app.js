9e91e8d66a8c7f9bbbf94263449ae1ab7f3e30d1
meads58 <meads109@gmail.com>
2015-04-09 19:27:36 +0100

    basic colours

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ var topapp = (function()
 
 	function onDeviceReady()
 	{
+    ble = evothings.ble;
 		startScan();
 		updateTimer = setInterval(displayBeaconList, 1000);
 	}
 
 	function startScan()
 	{
+    evothings.ble.startScan()
 		function onBeaconsRanged(beaconInfo)
 		{
 			for (var i in beaconInfo.beacons)
@@ -45,25 +47,25 @@ var topapp = (function()
 
 	function displayBeaconList()
 	{
-		testArray.push(beacons)
+
 
 		$.each(beacons, function(key, beacon) {
 
-			
+
 			if(beacon.distance < 0.2) {
 
 					if(beacon.major == 8981 && beacon.minor == 49281) {
 						$('body').css('background-color', 'green');
-					} 
+					}
 					if (beacon.major == 33613 && beacon.minor == 1285) {
-						$('body').css('background-color', 'blue');
-					} 
+             $('body').css('background-color', 'blue');
+					}
 					if (beacon.major == 56449 && beacon.minor == 6595) {
 						$('body').css('background-color', 'purple');
 					}
 					if (beacon.major == 4851 && beacon.minor == 360) {
 						$('body').css('background-color', '#00FF00');
-					} 
+					}
 					if (beacon.major == 23124 && beacon.minor == 41840) {
 						$('body').css('background-color', '#3366FF');
 					}
